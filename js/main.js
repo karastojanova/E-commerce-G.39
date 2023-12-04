ae064d772b1ff23cde599f158e701866718eb045
enomorino <enesherifi5@gmail.com>
2023-12-04 14:18:34 +0100

    fixed cart-summary

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-function toggleDropdown() {
-    var dropdown = document.getElementById("myDropdown");
-    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
-}
+
 
 
 
